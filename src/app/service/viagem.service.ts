45ada6fee0f8d18977095f834ea4b9a7b0907b99
Nicolas-NL <you@example.com>
2023-11-14 11:03:01 -0300

    Iniciado 14-11-2023

---
+++ src/app/service/viagem.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http'
+import { Observable } from 'rxjs';
+@Injectable({
+  providedIn: 'root'
+})
+export class ViagemService {
+
+  private API = '//localhost:8080/viagens';
+
+  constructor(private http : HttpClient) { }
+
+  listar() : Observable<any>{
+    return this.http.get(this.API)
+  }
+}
